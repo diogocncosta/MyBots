616cded0233079d252d2097f69d52a12d4b7476f
diogocncosta <diogocosta2023@u.northwestern.edu>
2023-01-08 15:40:48 -0600

    optimize code

--- generate.py
+++ generate.py
@@ -10,10 +10,6 @@ x = -2
 y = -2
 z = 0.5
 
-#this represent the position on where the robot will appear initially 
-x1 = 0
-y1 = 0
-z1 = 0.5
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -34,33 +30,8 @@ def Create_Robot():
     pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0, 1.0, 0])
 
     pyrosim.Send_Cube(name="BackLeg", pos=[0, 0.5, -0.5] , size=[length, width, height])
-    
-    """
-    pyrosim.Send_Cube(name="Link0", pos=[x1, y1, z1] , size=[length, width, height])
-
-    pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0, 0, 1.0])
-
-    pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5] , size=[length, width, height])
-
-    pyrosim.Send_Joint(name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1.0])
-
-    pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5] , size=[length, width, height])
-
-    pyrosim.Send_Joint(name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0, 0.5, 0.5])
-
-    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0] , size=[length, width, height])
-
-    """
-    
+   
     pyrosim.End()
 
 Create_World()
 Create_Robot()
-"""
-for i in range(10):
-    for j in range(5):
-        for l in range(5):
-            pyrosim.Send_Cube(name="Box", pos=[x+l,y+j,z+i] , size=[length*0.90**i, width*0.90**i, height*0.90**i])
-"""    
-
-pyrosim.End()
